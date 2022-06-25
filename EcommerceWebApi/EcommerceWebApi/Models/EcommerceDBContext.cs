c8b67aed84941ab0cf2fcda1d14a1ffa6b563776
shubhangitayade2 <105914219+shubhangitayade2@users.noreply.github.com>
2022-06-25 15:33:32 +0530

    Add files via upload

---
+++ EcommerceWebApi/EcommerceWebApi/Models/EcommerceDBContext.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace EcommerceWebApi.Models
+{
+    public partial class EcommerceDBContext : DbContext
+    {
+        public EcommerceDBContext()
+        {
+        }
+
+        public EcommerceDBContext(DbContextOptions<EcommerceDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblCategory> TblCategories { get; set; }
+        public virtual DbSet<TblLogin> TblLogins { get; set; }
+        public virtual DbSet<TblProduct> TblProducts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblCategory>(entity =>
+            {
+                entity.ToTable("TblCategory");
+
+                entity.Property(e => e.CatName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblLogin>(entity =>
+            {
+                entity.ToTable("TblLogin");
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TblProduct>(entity =>
+            {
+                entity.ToTable("TblProduct");
+
+                entity.Property(e => e.ProductDescription)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProductDiscount).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.ProductFinal).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.ProductImage)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProductMrp).HasColumnType("decimal(18, 0)");
+
+                entity.Property(e => e.ProductName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
